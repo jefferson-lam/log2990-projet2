9cef7a34363aea53d24396aef7a38fdd53ee8d5d
Hee-MinYang <heemin99@gmail.com>
2021-01-28 22:07:50 -0500

    rectangle: added shift capacity and base for generic tool

--- client/src/app/components/drawing/drawing.component.ts
+++ client/src/app/components/drawing/drawing.component.ts
@@ -3,6 +3,7 @@ import { Tool } from '@app/classes/tool';
 import { Vec2 } from '@app/classes/vec2';
 import { DrawingService } from '@app/services/drawing/drawing.service';
 import { PencilService } from '@app/services/tools/pencil-service';
+import { RectangleService } from '@app/services/tools/rectangle-service';
 
 // TODO : Avoir un fichier séparé pour les constantes ?
 export const DEFAULT_WIDTH = 1000;
@@ -25,8 +26,8 @@ export class DrawingComponent implements AfterViewInit {
     // TODO : Avoir un service dédié pour gérer tous les outils ? Ceci peut devenir lourd avec le temps
     private tools: Tool[];
     currentTool: Tool;
-    constructor(private drawingService: DrawingService, pencilService: PencilService) {
-        this.tools = [pencilService];
+    constructor(private drawingService: DrawingService, pencilService: PencilService, rectangleService: RectangleService) {
+        this.tools = [pencilService, rectangleService];
         this.currentTool = this.tools[0];
     }
 
@@ -51,6 +52,28 @@ export class DrawingComponent implements AfterViewInit {
     @HostListener('mouseup', ['$event'])
     onMouseUp(event: MouseEvent): void {
         this.currentTool.onMouseUp(event);
+    }
+
+    @HostListener('keydown', ['$event'])
+    onKeyDown(event: KeyboardEvent): void {
+        this.currentTool.onKeyDown(event);
+    }
+
+    @HostListener('keyup', ['$event'])
+    onKeyUp(event: KeyboardEvent): void {
+        this.currentTool.onKeyUp(event);
+    }
+
+    @HostListener('keypress', ['$event'])
+    // TODO: MODIFY ONCE WE HAVE TOOL MANAGER
+    onKeyToolSwitch(event: KeyboardEvent): void {
+        if (event.key == '1') {
+            // change later when tools aren't managed by this.currentTool anymore
+            this.currentTool = this.tools[1];
+        } else if (event.key == 'c') {
+            // change later when tools aren't managed by this.currentTool anymore
+            this.currentTool = this.tools[0];
+        }
     }
 
     get width(): number {
