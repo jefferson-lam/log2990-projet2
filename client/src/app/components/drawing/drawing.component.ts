4d28eb53fa494fba6edc9c28246e98bc5605e02c
jefferson-lam <jefferson.lam@polymtl.ca>
2021-01-31 21:51:23 -0500

    Set up line feature as a service, and completed preliminary line drawing feature with clicks

--- client/src/app/components/drawing/drawing.component.ts
+++ client/src/app/components/drawing/drawing.component.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component, ElementRef, HostListener, ViewChild } from '@
 import { Tool } from '@app/classes/tool';
 import { Vec2 } from '@app/classes/vec2';
 import { DrawingService } from '@app/services/drawing/drawing.service';
+import { LineService } from '@app/services/tools/line-service';
 import { PencilService } from '@app/services/tools/pencil-service';
 
 // TODO : Avoir un fichier séparé pour les constantes ?
@@ -25,9 +26,9 @@ export class DrawingComponent implements AfterViewInit {
     // TODO : Avoir un service dédié pour gérer tous les outils ? Ceci peut devenir lourd avec le temps
     private tools: Tool[];
     currentTool: Tool;
-    constructor(private drawingService: DrawingService, pencilService: PencilService) {
-        this.tools = [pencilService];
-        this.currentTool = this.tools[0];
+    constructor(private drawingService: DrawingService, pencilService: PencilService, lineService: LineService) {
+        this.tools = [pencilService, lineService];
+        this.currentTool = this.tools[1];
     }
 
     ngAfterViewInit(): void {
@@ -36,6 +37,26 @@ export class DrawingComponent implements AfterViewInit {
         this.drawingService.baseCtx = this.baseCtx;
         this.drawingService.previewCtx = this.previewCtx;
         this.drawingService.canvas = this.baseCanvas.nativeElement;
+    }
+
+    @HostListener('keyboard', ['$event'])
+    onKeyboardDown(event: KeyboardEvent): void {
+        this.currentTool.onKeyboardDown(event);
+    }
+
+    @HostListener('keypress', ['$event'])
+    onKeyboardPress(event: KeyboardEvent): void {
+        this.currentTool.onKeyboardPress(event);
+    }
+
+    @HostListener('click', ['$event'])
+    onMouseClick(event: MouseEvent): void {
+        this.currentTool.onMouseClick(event);
+    }
+
+    @HostListener('dblclick', ['$event'])
+    onMouseDoubleClick(event: MouseEvent): void {
+        this.currentTool.onMouseDoubleClick(event);
     }
 
     @HostListener('mousemove', ['$event'])
