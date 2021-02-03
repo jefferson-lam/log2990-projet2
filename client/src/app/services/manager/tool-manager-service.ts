8e57a043770012434822cb90d795d462626ca8fa
jefferson-lam <jefferson.lam@polymtl.ca>
2021-02-03 14:15:35 -0500

    Fixed integration with tool manager

--- client/src/app/services/manager/tool-manager-service.ts
+++ client/src/app/services/manager/tool-manager-service.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Tool } from '@app/classes/tool';
+import { LineService } from '@app/services/tools/line-service';
 import { PencilService } from '@app/services/tools/pencil-service';
 
 @Injectable({
     providedIn: 'root',
 })
 export class ToolManagerService {
-    constructor(public pencilService: PencilService) {}
+    constructor(public pencilService: PencilService, public lineService: LineService) {}
 
-    // Method that will pick tool according to what user presses on keyboard
     selectTool(event: KeyboardEvent): Tool {
+        console.log('Changing tool in tool manager...');
         switch (event.key) {
             case 'c':
                 return this.pencilService;
+            case 'l':
+                return this.lineService;
             default:
                 return this.pencilService;
         }
