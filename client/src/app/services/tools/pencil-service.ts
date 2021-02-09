988085313af6ba76c41c8a3e0158c9225d17cc04
Nathan <nathan.ramsay-vejlens@polymtl.ca>
2021-02-09 13:00:23 -0500

    PencilService: onMouseLeave and Enter implementations

--- client/src/app/services/tools/pencil-service.ts
+++ client/src/app/services/tools/pencil-service.ts
@@ -2,8 +2,11 @@ import { Injectable } from '@angular/core';
 import { Tool } from '@app/classes/tool';
 import { Vec2 } from '@app/classes/vec2';
 import { DrawingService } from '@app/services/drawing/drawing.service';
+import { Observable, Subject } from 'rxjs';
 
 // TODO : Déplacer ça dans un fichier séparé accessible par tous
+export const MIN_SIZE_PENCIL = 1;
+export const MAX_SIZE_PENCIL = 200;
 export enum MouseButton {
     Left = 0,
     Middle = 1,
@@ -21,10 +24,27 @@ export enum MouseButton {
 })
 export class PencilService extends Tool {
     private pathData: Vec2[];
+    size: number;
+
+    // Observables
+    private pencilSizeChangedSource: Subject<number> = new Subject<number>();
+    pencilSizeChanged$: Observable<number> = this.pencilSizeChangedSource.asObservable();
 
     constructor(drawingService: DrawingService) {
         super(drawingService);
         this.clearPath();
+        this.size = MIN_SIZE_PENCIL;
+    }
+
+    setSize(value: number): void {
+        if (value >= MIN_SIZE_PENCIL && value <= MAX_SIZE_PENCIL) {
+            this.size = value;
+        } else if (value >= MAX_SIZE_PENCIL) {
+            this.size = MAX_SIZE_PENCIL;
+        } else {
+            this.size = MIN_SIZE_PENCIL;
+        }
+        this.pencilSizeChangedSource.next(this.size);
     }
 
     onMouseDown(event: MouseEvent): void {
@@ -59,8 +79,25 @@ export class PencilService extends Tool {
         }
     }
 
+    onMouseLeave(event: MouseEvent): void {
+        if (this.mouseDown) {
+            this.drawLine(this.drawingService.baseCtx, this.pathData);
+            this.drawingService.clearCanvas(this.drawingService.previewCtx);
+        }
+        this.clearPath();
+    }
+
+    onMouseEnter(event: MouseEvent): void {
+        if (this.mouseDown && event.buttons === MouseButton.Left) {
+            this.mouseDown = false;
+        }
+    }
+
     private drawLine(ctx: CanvasRenderingContext2D, path: Vec2[]): void {
         ctx.beginPath();
+        ctx.lineWidth = this.size;
+        ctx.lineCap = 'round';
+        ctx.lineJoin = 'round';
         for (const point of path) {
             ctx.lineTo(point.x, point.y);
         }
