e32fa2c67202c8cf91cb4d03d9e30aca838b92d9
Hee-MinYang <hee-min.yang@polymtl.ca>
2021-02-15 00:02:07 -0500

    Added line service methods that will change line service params

--- client/src/app/services/manager/settings-manager.ts
+++ client/src/app/services/manager/settings-manager.ts
@@ -10,15 +10,24 @@ export class SettingsManagerService {
     fillMode: FillMode;
     editorComponent: EditorComponent;
 
-    changeSizeTool(newWidth: number): void {
+    setLineWidth(newWidth: number): void {
         this.editorComponent.currentTool.setLineWidth(newWidth);
     }
 
-    changeFillMode(newFillMode: ToolConstants.FillMode): void {
+    setFillMode(newFillMode: ToolConstants.FillMode): void {
         this.editorComponent.currentTool.setFillMode(newFillMode);
     }
+
+    setJunctionRadius(newJunctionRadius: number): void {
+        this.editorComponent.currentTool.setJunctionRadius(newJunctionRadius);
+    }
+
+    setWithJunction(hasJunction: boolean): void {
+        this.editorComponent.currentTool.setWithJunction(hasJunction);
+    }
+
     // TODO: Put these lines in the component containing the event emitter
     // @Output() eraserSizeChanged: EventEmitter<number> = new EventEmitter(); // in the attributes
-    //  this.eraserSizeChanged.subscribe((newSize: number) => settingsManager.changeSizeTools(newSize)); // in the constructor
+    //  this.eraserSizeChanged.subscribe((newSize: number) => settingsManager.setLineWidths(newSize)); // in the constructor
     // this.eraserSizeChanged.emit(this.size); // when the size has changed
 }
