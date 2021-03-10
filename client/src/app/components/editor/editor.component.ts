249c7de65b01ccff1e1bdd5e90a5ecc26fd3c5b7
jefferson-lam <jefferson.lam@polymtl.ca>
2021-03-10 17:04:00 -0500

    Added key to editor for tool change

--- client/src/app/components/editor/editor.component.ts
+++ client/src/app/components/editor/editor.component.ts
@@ -58,7 +58,7 @@ export class EditorComponent implements OnInit {
                     default:
                         break;
                 }
-            } else if (event.key.match(/^(1|2|3|c|l|e)$/)) {
+            } else if (event.key.match(/^(1|2|3|a|c|l|e)$/)) {
                 this.currentTool = this.toolManager.selectTool(event);
             }
         }
