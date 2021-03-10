496d7a20c10f100d3decbc8ebf07dfe0a4d9f0a0
jefferson-lam <jefferson.lam@polymtl.ca>
2021-03-10 17:44:06 -0500

    Revert "Revert "Merge branch 'feature/polygone-only' into 'dev'""
    This reverts commit f1d6e1a491b3f93b71c900d8957a0d5c20631291.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -32,6 +32,7 @@ import { SidebarEllipseComponent } from './components/sidebar/sidebar-tools-opti
 import { SidebarEraserComponent } from './components/sidebar/sidebar-tools-options-2.0/sidebar-eraser/sidebar-eraser.component';
 import { SidebarLineComponent } from './components/sidebar/sidebar-tools-options-2.0/sidebar-line/sidebar-line.component';
 import { SidebarPencilComponent } from './components/sidebar/sidebar-tools-options-2.0/sidebar-pencil/sidebar-pencil.component';
+import { SidebarPolygoneComponent } from './components/sidebar/sidebar-tools-options-2.0/sidebar-polygone/sidebar-polygone.component';
 import { SidebarRectangleComponent } from './components/sidebar/sidebar-tools-options-2.0/sidebar-rectangle/sidebar-rectangle.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
@@ -51,6 +52,7 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
         SidebarRectangleComponent,
         SidebarEllipseComponent,
         SidebarLineComponent,
+        SidebarPolygoneComponent,
     ],
     entryComponents: [MainPageCarrouselComponent],
     imports: [
