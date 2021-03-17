5fc4e84e2c538e1c1c9e5f554914ecef43813541
jefferson-lam <jefferson.lam@polymtl.ca>
2021-03-17 02:10:13 -0400

    Initial test setup for directional directive

--- client/src/app/components/selection/selection-directives/directional-movement.directive.spec.ts
+++ client/src/app/components/selection/selection-directives/directional-movement.directive.spec.ts
@@ -1,8 +1,18 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { SelectionComponent } from '../selection.component';
 import { DirectionalMovementDirective } from './directional-movement.directive';
 
 describe('DirectionalMovementDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DirectionalMovementDirective();
-    expect(directive).toBeTruthy();
-  });
+    let fixture: ComponentFixture<SelectionComponent>;
+    beforeEach(() => {
+        fixture = TestBed.configureTestingModule({
+            declarations: [SelectionComponent],
+        }).createComponent(SelectionComponent);
+        fixture.detectChanges();
+    });
+    it('should create an instance', () => {
+        const selectionCanvas = fixture.nativeElement.querySelector('#selectionLayer');
+        const directive = new DirectionalMovementDirective(selectionCanvas);
+        expect(directive).toBeTruthy();
+    });
 });
