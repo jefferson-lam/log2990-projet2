89eb8a999cfdfa2a8d41dde91357538f6e3d9646
Nathan <nathanrv98@gmail.com>
2021-03-17 20:01:56 -0400

    DrawingComponent: test canvas background white

--- client/src/app/components/drawing/drawing.component.spec.ts
+++ client/src/app/components/drawing/drawing.component.spec.ts
@@ -1,6 +1,7 @@
 import { SimpleChange } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { Tool } from '@app/classes/tool';
+import { WHITE_RGBA_DECIMAL } from '@app/constants/color-constants';
 import { DrawingService } from '@app/services/drawing/drawing.service';
 import { EraserService } from '@app/services/tools/eraser/eraser-service';
 import { LineService } from '@app/services/tools/line/line-service';
@@ -52,8 +53,8 @@ describe('DrawingComponent', () => {
     it('should initialize baseCanvas with a white background', () => {
         const baseCtx: CanvasRenderingContext2D = component.baseCanvas.nativeElement.getContext('2d') as CanvasRenderingContext2D;
         const pixelBuffer = new Uint32Array(baseCtx.getImageData(0, 0, component.baseWidth, component.baseHeight).data.buffer);
-        const isEmpty = !pixelBuffer.some((color) => color !== 0);
-        expect(isEmpty).toBeFalsy();
+        const isWhite = !pixelBuffer.some((color) => color !== WHITE_RGBA_DECIMAL);
+        expect(isWhite).toBeTrue();
     });
 
     it('should have a default WIDTH and HEIGHT', () => {
