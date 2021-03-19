db49c0f7e192f1b4c7e4fa37b6f009b2cc7258f9
jefferson-lam <jefferson.lam@polymtl.ca>
2021-03-19 03:50:20 -0400

    Removed sidebar component from app module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -31,7 +31,6 @@ import { SidebarEllipseComponent } from './components/sidebar/sidebar-tools-opti
 import { SidebarEraserComponent } from './components/sidebar/sidebar-tools-options-2.0/sidebar-eraser/sidebar-eraser.component';
 import { SidebarLineComponent } from './components/sidebar/sidebar-tools-options-2.0/sidebar-line/sidebar-line.component';
 import { SidebarPencilComponent } from './components/sidebar/sidebar-tools-options-2.0/sidebar-pencil/sidebar-pencil.component';
-import { SidebarRectangleSelectionComponent } from './components/sidebar/sidebar-tools-options-2.0/sidebar-rectangle-selection/sidebar-rectangle-selection/sidebar-rectangle-selection.component';
 import { SidebarRectangleComponent } from './components/sidebar/sidebar-tools-options-2.0/sidebar-rectangle/sidebar-rectangle.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
@@ -50,7 +49,6 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
         SidebarRectangleComponent,
         SidebarEllipseComponent,
         SidebarLineComponent,
-        SidebarRectangleSelectionComponent,
         SelectionComponent,
         DirectionalMovementDirective,
     ],
