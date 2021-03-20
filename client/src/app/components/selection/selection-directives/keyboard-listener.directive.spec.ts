8bd982b0deed61e790166ab441bb461fb0c2d0ce
jefferson-lam <jefferson.lam@polymtl.ca>
2021-03-19 23:36:30 -0400

    TODO: test keyboardlistener directive

--- client/src/app/components/selection/selection-directives/keyboard-listener.directive.spec.ts
+++ client/src/app/components/selection/selection-directives/keyboard-listener.directive.spec.ts
@@ -1,8 +1,19 @@
+import { TestBed } from '@angular/core/testing';
+import { ToolManagerService } from '@app/services/manager/tool-manager-service';
 import { KeyboardListenerDirective } from './keyboard-listener.directive';
 
+// class ToolStub extends Tool {}
+
 describe('KeyboardListenerDirective', () => {
-  it('should create an instance', () => {
-    const directive = new KeyboardListenerDirective();
-    expect(directive).toBeTruthy();
-  });
+    let toolManager: ToolManagerService;
+    // let toolStub: ToolStub;
+    beforeEach(() => {
+        // toolStub = new ToolStub({} as DrawingService, {} as UndoRedoService);
+        toolManager = TestBed.inject(ToolManagerService);
+    });
+
+    it('should create an instance', () => {
+        const directive = new KeyboardListenerDirective(toolManager);
+        expect(directive).toBeTruthy();
+    });
 });
