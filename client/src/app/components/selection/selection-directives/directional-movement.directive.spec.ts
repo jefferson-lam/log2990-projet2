9b1eebb1a7d225d62b15396346c7ecf6587afae7
jefferson-lam <jefferson.lam@polymtl.ca>
2021-03-20 18:59:46 -0400

    Updated tests to cover new functions

--- client/src/app/components/selection/selection-directives/directional-movement.directive.spec.ts
+++ client/src/app/components/selection/selection-directives/directional-movement.directive.spec.ts
@@ -1,14 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SelectionComponent } from '@app/components/selection/selection.component';
-import { ResizerHandlerService } from '@app/services/resizer/resizer-handler.service';
 import { DirectionalMovementDirective } from './directional-movement.directive';
 
 describe('DirectionalMovementDirective', () => {
     let fixture: ComponentFixture<SelectionComponent>;
-    let resizerHandlerService: ResizerHandlerService;
 
     beforeEach(() => {
-        resizerHandlerService = TestBed.inject(ResizerHandlerService);
         fixture = TestBed.configureTestingModule({
             declarations: [SelectionComponent],
         }).createComponent(SelectionComponent);
@@ -17,7 +14,7 @@ describe('DirectionalMovementDirective', () => {
 
     it('should create an instance', () => {
         const selectionCanvas = fixture.nativeElement.querySelector('#selectionLayer');
-        const directive = new DirectionalMovementDirective(selectionCanvas, resizerHandlerService);
+        const directive = new DirectionalMovementDirective(selectionCanvas);
         expect(directive).toBeTruthy();
     });
 });
