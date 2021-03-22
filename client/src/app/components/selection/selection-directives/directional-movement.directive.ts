28f0888c848d1ede5fa8ed66622f9aa3c349b001
Hee-MinYang <hee-min.yang@polymtl.ca>
2021-03-21 21:23:43 -0400

    Added implementation of dekayed movement

--- client/src/app/components/selection/selection-directives/directional-movement.directive.ts
+++ client/src/app/components/selection/selection-directives/directional-movement.directive.ts
@@ -2,6 +2,8 @@ import { Directive, ElementRef, EventEmitter, HostListener, Output } from '@angu
 import { ResizerHandlerService } from '@app/services/resizer/resizer-handler.service';
 
 const NUM_PIXELS = 3;
+const FIRST_PRESS_DELAY_MS = 500;
+const CONTINUOUS_PRESS_DELAY_MS = 100;
 @Directive({
     selector: '[appDirectionalMovement]',
     providers: [ResizerHandlerService],
@@ -9,35 +11,42 @@ const NUM_PIXELS = 3;
 export class DirectionalMovementDirective {
     keyPressed: Map<string, number> = new Map();
     leftMovement: number;
+    private hasFirstClickHappened: boolean = false;
+    private hasMovedOnce: boolean = false;
     @Output() onCanvasMovement = new EventEmitter();
 
     constructor(private element: ElementRef) {}
 
     @HostListener('keydown', ['$event'])
-    onKeyboardDown(event: KeyboardEvent): void {
+    async onKeyboardDown(event: KeyboardEvent): Promise<void> {
         event.preventDefault();
+
         if (!this.keyPressed.get(event.key)) {
             // First press
             this.keyPressed.set(event.key, event.timeStamp);
+            this.translateSelection();
+            await this.delay(FIRST_PRESS_DELAY_MS);
+            this.hasFirstClickHappened = true;
         }
-        if (this.keyPressed.get('ArrowLeft')) {
-            this.translateLeft(NUM_PIXELS);
+
+        if (this.hasMovedOnce) {
+            return;
         }
-        if (this.keyPressed.get('ArrowUp')) {
-            this.translateUp(NUM_PIXELS);
+
+        if (this.hasFirstClickHappened) {
+            if (!this.hasMovedOnce) {
+                this.hasMovedOnce = true;
+                await this.delay(CONTINUOUS_PRESS_DELAY_MS);
+                this.translateSelection();
+                this.hasMovedOnce = false;
+            }
         }
-        if (this.keyPressed.get('ArrowRight')) {
-            this.translateRight(NUM_PIXELS);
-        }
-        if (this.keyPressed.get('ArrowDown')) {
-            this.translateDown(NUM_PIXELS);
-        }
-        this.onCanvasMovement.emit(true);
     }
 
     @HostListener('keyup', ['$event'])
     onKeyboardUp(event: KeyboardEvent): void {
         this.keyPressed.set(event.key, 0);
+        this.hasFirstClickHappened = false;
     }
 
     translateLeft(numPixels: number): void {
@@ -54,5 +63,25 @@ export class DirectionalMovementDirective {
 
     translateDown(numPixels: number): void {
         this.element.nativeElement.style.top = parseInt(this.element.nativeElement.style.top, 10) + numPixels + 'px';
+    }
+
+    private delay(ms: number): Promise<void> {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+
+    private translateSelection(): void {
+        if (this.keyPressed.get('ArrowLeft')) {
+            this.translateLeft(NUM_PIXELS);
+        }
+        if (this.keyPressed.get('ArrowUp')) {
+            this.translateUp(NUM_PIXELS);
+        }
+        if (this.keyPressed.get('ArrowRight')) {
+            this.translateRight(NUM_PIXELS);
+        }
+        if (this.keyPressed.get('ArrowDown')) {
+            this.translateDown(NUM_PIXELS);
+        }
+        this.onCanvasMovement.emit(true);
     }
 }
