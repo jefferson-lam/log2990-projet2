0f84b8c7504e6d6c0276016ac1a516613ec48141
jefferson-lam <jefferson.lam@polymtl.ca>
2021-03-22 02:36:53 -0400

    Added ellipse selection sidebar component

---
+++ client/src/app/components/sidebar/sidebar-tools-options-2.0/sidebar-ellipse-selection/sidebar-ellipse-selection/sidebar-ellipse-selection.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-sidebar-ellipse-selection',
+    templateUrl: './sidebar-ellipse-selection.component.html',
+    styleUrls: ['./sidebar-ellipse-selection.component.scss'],
+})
+export class SidebarEllipseSelectionComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
