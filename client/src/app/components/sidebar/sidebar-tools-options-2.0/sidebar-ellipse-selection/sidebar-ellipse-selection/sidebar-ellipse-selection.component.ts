c42f58d14c8813575b3bd15971410beb02a2dfba
jefferson-lam <jefferson.lam@polymtl.ca>
2021-03-23 01:30:32 -0400

    Fixed linting issues

--- client/src/app/components/sidebar/sidebar-tools-options-2.0/sidebar-ellipse-selection/sidebar-ellipse-selection/sidebar-ellipse-selection.component.ts
+++ client/src/app/components/sidebar/sidebar-tools-options-2.0/sidebar-ellipse-selection/sidebar-ellipse-selection/sidebar-ellipse-selection.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-sidebar-ellipse-selection',
     templateUrl: './sidebar-ellipse-selection.component.html',
     styleUrls: ['./sidebar-ellipse-selection.component.scss'],
 })
-export class SidebarEllipseSelectionComponent implements OnInit {
-    constructor() {}
-
-    ngOnInit(): void {}
-}
+export class SidebarEllipseSelectionComponent {}
--- client/src/app/components/sidebar/sidebar-tools-options-2.0/sidebar-rectangle-selection/sidebar-rectangle-selection/sidebar-rectangle-selection.component.ts
+++ client/src/app/components/sidebar/sidebar-tools-options-2.0/sidebar-rectangle-selection/sidebar-rectangle-selection/sidebar-rectangle-selection.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-sidebar-rectangle-selection',
     templateUrl: './sidebar-rectangle-selection.component.html',
     styleUrls: ['./sidebar-rectangle-selection.component.scss'],
 })
-export class SidebarRectangleSelectionComponent implements OnInit {
-    constructor() {}
-
-    ngOnInit(): void {}
-}
+export class SidebarRectangleSelectionComponent {}
