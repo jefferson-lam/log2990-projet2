f26bc055240f35cbe85514ef8fcd24ed14d6b17b
Hee-MinYang <hee-min.yang@polymtl.ca>
2021-03-23 23:52:21 -0400

    removed unsused server classes

--- server/app/inversify.config.ts
+++ server/app/inversify.config.ts
@@ -1,11 +1,7 @@
-import { DateController } from '@app/controllers/date.controller';
 import { DrawingsDatabaseController } from '@app/controllers/drawings-database.controller';
-import { IndexController } from '@app/controllers/index.controller';
 import { DrawingsDatabaseService } from '@app/services/database/drawings-database.service';
 import { TagValidatorService } from '@app/services/database/tag-validator/tag-validator.service';
 import { TitleValidatorService } from '@app/services/database/title-validator/title-validator.service';
-import { DateService } from '@app/services/date.service';
-import { IndexService } from '@app/services/index.service';
 import { Container } from 'inversify';
 import { Application } from './app';
 import { LocalDrawingsController } from './controllers/local-drawings.controller';
@@ -18,11 +14,6 @@ export const containerBootstrapper: () => Promise<Container> = async () => {
 
     container.bind(TYPES.Server).to(Server);
     container.bind(TYPES.Application).to(Application);
-    container.bind(TYPES.IndexController).to(IndexController);
-    container.bind(TYPES.IndexService).to(IndexService);
-
-    container.bind(TYPES.DateController).to(DateController);
-    container.bind(TYPES.DateService).to(DateService);
 
     container.bind(TYPES.DrawingsDatabaseController).to(DrawingsDatabaseController);
     container.bind(TYPES.DrawingsDatabaseService).to(DrawingsDatabaseService);
--- server/app/services/database/validator/validator.service.ts
+++ server/app/services/database/validator/validator.service.ts
@@ -1,6 +1,7 @@
 import { Message } from '@common/communication/message';
 import * as DatabaseConstants from '@common/validation/database-constants';
 import { injectable } from 'inversify';
+import 'reflect-metadata';
 
 @injectable()
 export class ValidatorService {
