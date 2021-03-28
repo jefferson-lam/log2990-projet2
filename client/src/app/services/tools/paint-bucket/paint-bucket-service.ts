9d52b025433215d97a91ec740036880fc03e71ba
jefferson-lam <jefferson.lam@polymtl.ca>
2021-03-27 22:11:52 -0400

    Fast implementation of flood fill algorithm as Uint32

--- client/src/app/services/tools/paint-bucket/paint-bucket-service.ts
+++ client/src/app/services/tools/paint-bucket/paint-bucket-service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { PixelData } from '@app/classes/pixel-data';
 import { Tool } from '@app/classes/tool';
 import { DEFAULT_TOLERANCE_VALUE } from '@app/constants/paint-bucket-constants';
 import { DrawingService } from '@app/services/drawing/drawing.service';
@@ -24,8 +25,63 @@ export class PaintBucketService extends Tool {
     }
 
     onMouseDown(event: MouseEvent) {
-        console.log(this.toleranceValue);
-        console.log(this.primaryColor);
-        console.log(this.drawingService.baseCtx.getImageData(0, 0, 100, 100));
+        const fillColor = 4290107125;
+        // this.floodFill(this.drawingService.baseCtx, event.offsetX, event.offsetY, fillColor);
+        this.fill(this.drawingService.baseCtx, event.offsetX, event.offsetY, fillColor);
+    }
+
+    getPixel(pixelData: PixelData, x: number, y: number): number {
+        if (x < 0 || y < 0 || x >= pixelData.width || y >= pixelData.height) {
+            return -1;
+        } else {
+            return pixelData.data[y * pixelData.width + x];
+        }
+    }
+
+    floodFill(ctx: CanvasRenderingContext2D, x: number, y: number, fillColor: number) {
+        const imageData = ctx.getImageData(0, 0, this.drawingService.canvas.width, this.drawingService.canvas.height);
+        const pixelData: PixelData = {
+            width: imageData.width,
+            height: imageData.height,
+            data: new Uint32Array(imageData.data.buffer),
+        };
+        const targetColor = this.getPixel(pixelData, x, y);
+        console.log(targetColor);
+        if (fillColor != targetColor) {
+            const pixels = [x, y];
+            while (pixels.length > 0) {
+                const y = pixels.pop()!;
+                const x = pixels.pop()!;
+                const currentPixelColor = this.getPixel(pixelData, x, y);
+                if (currentPixelColor === targetColor) {
+                    pixelData.data[y * pixelData.width + x] = fillColor;
+                    pixels.push(x + 1, y);
+                    pixels.push(x - 1, y);
+                    pixels.push(x, y + 1);
+                    pixels.push(x, y - 1);
+                }
+            }
+        }
+        ctx.putImageData(imageData, 0, 0);
+    }
+
+    fill(ctx: CanvasRenderingContext2D, x: number, y: number, fillColor: number) {
+        const imageData = ctx.getImageData(0, 0, this.drawingService.canvas.width, this.drawingService.canvas.height);
+        const pixelData: PixelData = {
+            width: imageData.width,
+            height: imageData.height,
+            data: new Uint32Array(imageData.data.buffer),
+        };
+        const targetColor = this.getPixel(pixelData, x, y);
+        for (let y = 0; y < pixelData.height; y++) {
+            for (let x = 0; x < pixelData.width; x++) {
+                const currentPixelColor = this.getPixel(pixelData, x, y);
+                if (currentPixelColor === targetColor) {
+                    pixelData.data[y * pixelData.width + x] = fillColor;
+                }
+            }
+        }
+
+        ctx.putImageData(imageData, 0, 0);
     }
 }
