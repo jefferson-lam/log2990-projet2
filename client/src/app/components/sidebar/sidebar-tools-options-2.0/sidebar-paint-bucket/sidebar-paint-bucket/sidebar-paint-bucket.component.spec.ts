0d801d26d605d8204ade6988c6f3354820c72275
jefferson-lam <jefferson.lam@polymtl.ca>
2021-04-08 01:42:44 -0400

    Fix custom element schemas

--- client/src/app/components/sidebar/sidebar-tools-options-2.0/sidebar-paint-bucket/sidebar-paint-bucket/sidebar-paint-bucket.component.spec.ts
+++ client/src/app/components/sidebar/sidebar-tools-options-2.0/sidebar-paint-bucket/sidebar-paint-bucket/sidebar-paint-bucket.component.spec.ts
@@ -1,3 +1,4 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { SidebarPaintBucketComponent } from './sidebar-paint-bucket.component';
 
@@ -8,6 +9,7 @@ describe('SidebarPaintBucketComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [SidebarPaintBucketComponent],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA],
         }).compileComponents();
     }));
 
