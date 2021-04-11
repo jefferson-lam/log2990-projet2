2c2a1b32260ac06d923e9a3238e7c997caf327a0
macotu <maxime.cote.lapointe@gmail.com>
2021-04-11 18:12:21 -0400

    Added type for PostRequests needed after refactoring

--- client/src/app/constants/export-drawing-constants.ts
+++ client/src/app/constants/export-drawing-constants.ts
@@ -17,3 +17,9 @@ export const BAD_REQUEST = 400;
 export const EXPORT_PROGRESS = 0;
 
 export const URL = 1;
+
+export type PostRequest = {
+    method: string;
+    headers: Headers;
+    body: FormData;
+};
