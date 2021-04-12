d203aeb58706247f3c03b62b926c995e40e5eb9f
jefferson-lam <jefferson.lam@polymtl.ca>
2021-04-12 13:17:30 -0400

    Added attributes to lasso-selection class

--- client/src/app/services/tools/selection/lasso/lasso-selection.ts
+++ client/src/app/services/tools/selection/lasso/lasso-selection.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Vec2 } from '@app/classes/vec2';
+import * as SelectionConstants from '@app/constants/selection-constants';
 import { DrawingService } from '@app/services/drawing/drawing.service';
 import { ResizerHandlerService } from '@app/services/resizer/resizer-handler.service';
 import { UndoRedoService } from '@app/services/undo-redo/undo-redo.service';
@@ -9,6 +11,11 @@ import { ToolSelectionService } from '../tool-selection-service';
     providedIn: 'root',
 })
 export class LassoSelectionService extends ToolSelectionService {
+    isManipulating: boolean;
+    isConnected: boolean;
+    linePathData: Vec2[];
+    initialPoint: Vec2;
+
     constructor(
         drawingService: DrawingService,
         undoRedoService: UndoRedoService,
@@ -16,5 +23,23 @@ export class LassoSelectionService extends ToolSelectionService {
         public lineService: LineService,
     ) {
         super(drawingService, undoRedoService, resizerHandlerService, lineService);
+        this.isManipulating = false;
+        this.isConnected = false;
+        this.linePathData = new Array<Vec2>();
+    }
+
+    onMouseDown(event: MouseEvent): void {
+        if (!this.inUse) {
+            this.initialPoint = this.getPositionFromMouse(event);
+            this.linePathData[SelectionConstants.START_INDEX] = this.initialPoint;
+            this.linePathData.push(this.initialPoint);
+        } else {
+            this.linePathData.push(this.linePathData[this.linePathData.length - 1]);
+        }
+        super.onMouseDown(event);
+    }
+
+    onMouseMove(event: MouseEvent): void {
+        super.onMouseMove(event);
     }
 }
