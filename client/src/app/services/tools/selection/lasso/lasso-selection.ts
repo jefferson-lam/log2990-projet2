4a105bee62596e98e654ddaffa00d52e384425d7
jefferson-lam <jefferson.lam@polymtl.ca>
2021-04-12 16:03:33 -0400

    Corrected import reference

--- client/src/app/services/tools/selection/lasso/lasso-selection.ts
+++ client/src/app/services/tools/selection/lasso/lasso-selection.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Vec2 } from '@app/classes/vec2';
 import * as SelectionConstants from '@app/constants/selection-constants';
 import { DrawingService } from '@app/services/drawing/drawing.service';
-import { ResizerHandlerService } from '@app/services/resizer/resizer-handler.service';
+import { ResizerHandlerService } from '@app/services/tools/selection/resizer/resizer-handler.service';
 import { UndoRedoService } from '@app/services/undo-redo/undo-redo.service';
 import { LineService } from '../../line/line-service';
 import { ToolSelectionService } from '../tool-selection-service';
