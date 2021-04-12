f545d4a44204688c031e12d4936d47ec1e873702
macotu <maxime.cote.lapointe@gmail.com>
2021-04-12 15:33:28 -0400

    Added constants in rectangle-constants

--- client/src/app/constants/rectangle-constants.ts
+++ client/src/app/constants/rectangle-constants.ts
@@ -5,3 +5,13 @@ export const END_INDEX = 1;
 export const MIN_BORDER_WIDTH = 1;
 
 export const MAX_BORDER_WIDTH = 50;
+
+export const MAX_LINE_WIDTH = 200;
+
+export const MIN_LINE_WIDTH = 1;
+
+export const TICK_INTERVAL = 1;
+
+export const INITIAL_LINE_WIDTH = 30;
+
+export const DIMENSION = 2;
