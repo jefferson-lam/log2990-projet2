7350b4a52f58e92f3332c191b625d749d1a48c42
jefferson-lam <jefferson.lam@polymtl.ca>
2021-04-12 21:32:37 -0400

    Extracted cloneCanvas from selectionCommands to command

--- client/src/app/services/tools/selection/ellipse/ellipse-selection-command.ts
+++ client/src/app/services/tools/selection/ellipse/ellipse-selection-command.ts
@@ -54,16 +54,6 @@ export class EllipseSelectionCommand extends Command {
         this.ctx.restore();
     }
 
-    cloneCanvas(selectionCanvas: HTMLCanvasElement): HTMLCanvasElement {
-        const newCanvas = document.createElement('canvas');
-        const context = newCanvas.getContext('2d') as CanvasRenderingContext2D;
-        newCanvas.width = selectionCanvas.width;
-        newCanvas.height = selectionCanvas.height;
-        // apply the old canvas to the new one
-        context.drawImage(selectionCanvas, 0, 0);
-        return newCanvas;
-    }
-
     clipEllipse(ctx: CanvasRenderingContext2D, start: Vec2, height: number, width: number, offset: number): void {
         const end: Vec2 = {
             x: start.x + width,
--- client/src/app/services/tools/selection/rectangle/rectangle-selection-command.ts
+++ client/src/app/services/tools/selection/rectangle/rectangle-selection-command.ts
@@ -50,14 +50,4 @@ export class RectangleSelectionCommand extends Command {
             this.selectionHeight,
         );
     }
-
-    cloneCanvas(selectionCanvas: HTMLCanvasElement): HTMLCanvasElement {
-        const newCanvas = document.createElement('canvas');
-        const context = newCanvas.getContext('2d') as CanvasRenderingContext2D;
-        newCanvas.width = selectionCanvas.width;
-        newCanvas.height = selectionCanvas.height;
-        // apply the old canvas to the new one
-        context.drawImage(selectionCanvas, 0, 0);
-        return newCanvas;
-    }
 }
