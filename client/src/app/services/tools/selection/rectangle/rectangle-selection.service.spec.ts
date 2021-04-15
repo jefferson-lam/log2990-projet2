67ca55337dbef8ca79da2485535ebf341c1df9db
macotu <maxime.cote.lapointe@gmail.com>
2021-04-15 19:32:23 -0400

    Changed imports with new shapes-constants file in rectangle-selection service.spec.ts

--- client/src/app/services/tools/selection/rectangle/rectangle-selection.service.spec.ts
+++ client/src/app/services/tools/selection/rectangle/rectangle-selection.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { CanvasTestHelper } from '@app/classes/canvas-test-helper';
 import { Vec2 } from '@app/classes/vec2';
-import { END_INDEX, START_INDEX } from '@app/constants/ellipse-constants';
+import { END_INDEX, START_INDEX } from '@app/constants/shapes-constants';
 import { DrawingService } from '@app/services/drawing/drawing.service';
 import { ResizerHandlerService } from '@app/services/tools/selection/resizer/resizer-handler.service';
 import { ToolSelectionService } from '@app/services/tools/selection/tool-selection-service';
