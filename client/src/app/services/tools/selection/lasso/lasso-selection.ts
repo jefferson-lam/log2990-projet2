d9c159a818765879ed20c05b7a6aca5e2c5b7e3d
jefferson-lam <jefferson.lam@polymtl.ca>
2021-04-15 02:25:20 -0400

    Extract segment intersection logic into own service

--- client/src/app/services/tools/selection/lasso/lasso-selection.ts
+++ client/src/app/services/tools/selection/lasso/lasso-selection.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Command } from '@app/classes/command';
-import { Line2 } from '@app/classes/line2';
 import { Vec2 } from '@app/classes/vec2';
 import { MouseButton } from '@app/constants/mouse-constants';
 import * as SelectionConstants from '@app/constants/selection-constants';
 import { DrawingService } from '@app/services/drawing/drawing.service';
+import { SegmentIntersectionService } from '@app/services/helper/segment-intersection.service';
 import { LineService } from '@app/services/tools/line/line-service';
 import { LassoSelectionCommand } from '@app/services/tools/selection/lasso/lasso-selection-command';
 import { ResizerHandlerService } from '@app/services/tools/selection/resizer/resizer-handler.service';
@@ -30,6 +30,7 @@ export class LassoSelectionService extends ToolSelectionService {
         drawingService: DrawingService,
         undoRedoService: UndoRedoService,
         resizerHandlerService: ResizerHandlerService,
+        public segmentIntersectionService: SegmentIntersectionService,
         public lineService: LineService,
     ) {
         super(drawingService, undoRedoService, resizerHandlerService, lineService);
@@ -147,71 +148,12 @@ export class LassoSelectionService extends ToolSelectionService {
         let pathLine;
         for (let i = 0; i < pathData.length - 2; i++) {
             pathLine = { start: pathData[i], end: pathData[i + 1] };
-            const isIntersect = this.intersects(newLine, pathLine);
+            const isIntersect = this.segmentIntersectionService.intersects(newLine, pathLine);
             if (isIntersect) {
                 return true;
             }
         }
         return false;
-    }
-
-    // https://stackoverflow.com/questions/9043805/test-if-two-lines-intersect-javascript-function
-    private intersects(line1: Line2, line2: Line2): boolean {
-        let det;
-        let gamma;
-        let lambda;
-        det = (line1.end.x - line1.start.x) * (line2.end.y - line2.start.y) - (line2.end.x - line2.start.x) * (line1.end.y - line1.start.y);
-        if (this.isColinear(line1, line2)) {
-            return true;
-        }
-        if (det === 0) {
-            return false;
-        }
-        lambda =
-            ((line2.end.y - line2.start.y) * (line2.end.x - line1.start.x) + (line2.start.x - line2.end.x) * (line2.end.y - line1.start.y)) / det;
-        gamma = ((line1.start.y - line1.end.y) * (line2.end.x - line1.start.x) + (line1.end.x - line1.start.x) * (line2.end.y - line1.start.y)) / det;
-        return 0 < lambda && lambda < 1 && 0 < gamma && gamma < 1;
-    }
-
-    private isColinear(line1: Line2, line2: Line2): boolean {
-        const line3: Line2 = { start: line1.start, end: line2.start };
-        const slopeLine1 = this.calculateSlopeLine(line1);
-        const slopeLine3 = this.calculateSlopeLine(line3);
-        return slopeLine1 === slopeLine3 && this.doLinesShareRange(line1, line2);
-    }
-
-    private doLinesShareRange(line1: Line2, line2: Line2): boolean {
-        return (
-            (this.doDomainsOverlap(line1.start.x, line1.end.x, line2.start.x, line2.end.x) ||
-                this.areAllPointsAligned(line1.start.x, line1.end.x, line2.start.x, line2.end.x)) &&
-            (this.doDomainsOverlap(line1.start.y, line1.end.y, line2.start.y, line2.end.y) ||
-                this.areAllPointsAligned(line1.start.y, line1.end.y, line2.start.y, line2.end.y))
-        );
-    }
-
-    private doDomainsOverlap(line1Start: number, line1End: number, line2Start: number, line2End: number): boolean {
-        const minLine1 = Math.min(line1Start, line1End);
-        const maxLine1 = Math.max(line1Start, line1End);
-        const minLine2 = Math.min(line2Start, line2End);
-        const maxLine2 = Math.max(line2Start, line2End);
-        return (
-            (minLine1 >= minLine2 && minLine1 < maxLine2) ||
-            (maxLine1 > minLine2 && maxLine1 <= maxLine2) ||
-            (minLine2 >= minLine1 && minLine2 < maxLine1) ||
-            (maxLine2 > minLine1 && maxLine2 <= maxLine1)
-        );
-    }
-
-    private areAllPointsAligned(line1Start: number, line1End: number, line2Start: number, line2End: number): boolean {
-        return line1Start === line1End && line1Start === line2Start && line1Start === line2End;
-    }
-
-    private calculateSlopeLine(line: Line2): number {
-        const TOLERANCE = 2;
-        const ROUNDING_FACTOR = 10;
-        return Math.abs(line.end.x - line.start.x) < TOLERANCE
-            ? Number.POSITIVE_INFINITY
-            : Math.round((Math.abs(line.end.y - line.start.y) / Math.abs(line.end.x - line.start.x)) * ROUNDING_FACTOR) / ROUNDING_FACTOR;
     }
 
     computeSelectionSize(pathData: Vec2[]): number[] {
