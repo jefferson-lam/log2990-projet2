7d22ba35a770322592b8aee1bf98415ae4970c9d
jefferson-lam <jefferson.lam@polymtl.ca>
2021-04-16 03:20:20 -0400

    Fixed integration tests for lasso

--- client/src/app/services/tools/selection/lasso/lasso-selection.spec.ts
+++ client/src/app/services/tools/selection/lasso/lasso-selection.spec.ts
@@ -21,6 +21,7 @@ describe('LassoSelectionService', () => {
     let previewCtxStub: CanvasRenderingContext2D;
     let selectionCtxStub: CanvasRenderingContext2D;
     let previewSelectionCtxStub: CanvasRenderingContext2D;
+    let borderCtxStub: CanvasRenderingContext2D;
 
     let executeSpy: jasmine.Spy;
     let undoRedoService: UndoRedoService;
@@ -44,16 +45,19 @@ describe('LassoSelectionService', () => {
         previewCtxStub = canvasTestHelper.drawCanvas.getContext('2d') as CanvasRenderingContext2D;
         selectionCtxStub = canvasTestHelper.selectionCanvas.getContext('2d') as CanvasRenderingContext2D;
         previewSelectionCtxStub = canvasTestHelper.previewSelectionCanvas.getContext('2d') as CanvasRenderingContext2D;
+        borderCtxStub = canvasTestHelper.borderCanvas.getContext('2d') as CanvasRenderingContext2D;
 
         // tslint:disable:no-string-literal
         service['drawingService'].baseCtx = baseCtxStub; // Jasmine doesnt copy properties with underlying data
         service['drawingService'].previewCtx = previewCtxStub;
         service['drawingService'].selectionCtx = selectionCtxStub;
         service['drawingService'].previewSelectionCtx = previewSelectionCtxStub;
+        service['drawingService'].borderCtx = borderCtxStub;
 
         service['drawingService'].selectionCanvas = canvasTestHelper.selectionCanvas;
         service['drawingService'].canvas = canvasTestHelper.canvas;
         service['drawingService'].previewSelectionCanvas = canvasTestHelper.previewSelectionCanvas;
+        service['drawingService'].borderCanvas = canvasTestHelper.borderCanvas;
 
         executeSpy = spyOn(undoRedoService, 'executeCommand');
 
