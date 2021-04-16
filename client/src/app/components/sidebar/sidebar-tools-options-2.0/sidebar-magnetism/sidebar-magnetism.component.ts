5a9ba618e827a83ea3d5e7f9d7c52728fc6d280c
Hee-MinYang <hee-min.yang@polymtl.ca>
2021-04-16 16:08:12 -0400

    SideBarMagnetismComponent: Changed initialisation of event emitters to constructor

--- client/src/app/components/sidebar/sidebar-tools-options-2.0/sidebar-magnetism/sidebar-magnetism.component.ts
+++ client/src/app/components/sidebar/sidebar-tools-options-2.0/sidebar-magnetism/sidebar-magnetism.component.ts
@@ -14,10 +14,13 @@ export class SidebarMagnetismComponent implements OnInit {
     isMagnetismOn: boolean;
     MAGNETISM_CORNERS: typeof MagnestismConstants.MagnetizedPoint;
 
-    @Output() magnetismValueChanged: EventEmitter<boolean> = new EventEmitter();
-    @Output() referenceCornerChanged: EventEmitter<MagnestismConstants.MagnetizedPoint> = new EventEmitter();
+    @Output() magnetismValueChanged: EventEmitter<boolean>;
+    @Output() magnetizedPointChanged: EventEmitter<MagnestismConstants.MagnetizedPoint>;
 
-    constructor(public magnetismService: MagnetismService) {}
+    constructor(public magnetismService: MagnetismService) {
+        this.magnetismValueChanged = new EventEmitter();
+        this.magnetizedPointChanged = new EventEmitter();
+    }
 
     ngOnInit(): void {
         this.isMagnetismOn = this.magnetismService.isMagnetismOn;
@@ -25,7 +28,7 @@ export class SidebarMagnetismComponent implements OnInit {
         this.magnetismValueChanged.subscribe((isMagnetismOn: boolean) => {
             this.magnetismService.isMagnetismOn = isMagnetismOn;
         });
-        this.referenceCornerChanged.subscribe((newCorner: MagnestismConstants.MagnetizedPoint) => {
+        this.magnetizedPointChanged.subscribe((newCorner: MagnestismConstants.MagnetizedPoint) => {
             this.magnetismService.magnetizedPoint = newCorner;
         });
         this.magnetismService.magnetismStateSubject.asObservable().subscribe((magnetismState) => {
@@ -38,6 +41,6 @@ export class SidebarMagnetismComponent implements OnInit {
     }
 
     emitReferencePoint(referenceCorner: MagnestismConstants.MagnetizedPoint): void {
-        this.referenceCornerChanged.emit(referenceCorner);
+        this.magnetizedPointChanged.emit(referenceCorner);
     }
 }
