7c4ca06e036cb5cc280eae5b2199daaa3c0eb90e
agnessam <sycagnes@hotmail.com>
2021-04-18 00:02:43 -0400

    Ellipse, rectangle, polygone Commands: minimized drawTypeX function parameters

--- client/src/app/services/tools/polygone/polygone-command.ts
+++ client/src/app/services/tools/polygone/polygone-command.ts
@@ -1,6 +1,5 @@
 import { Command } from '@app/classes/command';
 import { Vec2 } from '@app/classes/vec2';
-import * as EllipseConstants from '@app/constants/ellipse-constants';
 import * as PolygoneConstants from '@app/constants/polygone-constants';
 import * as ToolConstants from '@app/constants/tool-constants';
 import { PolygoneService } from '@app/services/tools/polygone/polygone-service';
@@ -11,7 +10,12 @@ export class PolygoneCommand extends Command {
     fillMode: ToolConstants.FillMode;
     primaryColor: string;
     secondaryColor: string;
+    borderColor: string;
     cornerCoords: Vec2[] = [];
+
+    centerX: number;
+    centerY: number;
+    radiusWithin: number;
 
     constructor(canvasContext: CanvasRenderingContext2D, polygoneService: PolygoneService) {
         super();
@@ -19,7 +23,7 @@ export class PolygoneCommand extends Command {
     }
 
     execute(): void {
-        this.drawPolygone(this.ctx, this.cornerCoords, this.initNumberSides);
+        this.drawPolygone(this.ctx, this.cornerCoords);
     }
 
     setValues(canvasContext: CanvasRenderingContext2D, polygoneService: PolygoneService): void {
@@ -32,59 +36,48 @@ export class PolygoneCommand extends Command {
         Object.assign(this.cornerCoords, polygoneService.cornerCoords);
     }
 
-    private drawPolygone(ctx: CanvasRenderingContext2D, path: Vec2[], sides: number): void {
-        const polygoneCenter = this.getPolygoneCenter(path[PolygoneConstants.START_INDEX], path[PolygoneConstants.END_INDEX]);
-        const startX = polygoneCenter.x;
-        const startY = polygoneCenter.y;
-        const radiiXAndY = this.getRadiiXAndY(path);
-        const xRadius = radiiXAndY[EllipseConstants.X_INDEX];
-        const radiusWithin = xRadius - this.lineWidth / 2;
-        if (radiusWithin < 0) {
+    private drawPolygone(ctx: CanvasRenderingContext2D, path: Vec2[]): void {
+        this.getPolygoneCenter(path[PolygoneConstants.START_INDEX], path[PolygoneConstants.END_INDEX]);
+
+        const xRadius = this.getRadiiX(path);
+        this.radiusWithin = xRadius - this.lineWidth / 2;
+        if (this.radiusWithin < 0) {
             return;
         }
-        const borderColor: string = this.fillMode === ToolConstants.FillMode.FILL_ONLY ? this.primaryColor : this.secondaryColor;
-
-        this.drawTypePolygone(ctx, radiusWithin, startX, startY, sides, this.fillMode, this.primaryColor, borderColor, this.lineWidth);
+        this.borderColor = this.fillMode === ToolConstants.FillMode.FILL_ONLY ? this.primaryColor : this.secondaryColor;
+        this.drawTypePolygone(ctx);
     }
 
-    private drawTypePolygone(
-        ctx: CanvasRenderingContext2D,
-        radiusWithin: number,
-        startX: number,
-        startY: number,
-        sides: number,
-        fillMethod: number,
-        primaryColor: string,
-        secondaryColor: string,
-        lineWidth: number,
-    ): void {
-        const ANGLE_EVEN = PolygoneConstants.END_ANGLE / sides;
+    private drawTypePolygone(ctx: CanvasRenderingContext2D): void {
+        const ANGLE_EVEN = PolygoneConstants.END_ANGLE / this.initNumberSides;
         ctx.beginPath();
         ctx.lineJoin = 'round';
-        if (sides % 2 !== 0) {
-            for (let i = 0; i < sides; i++) {
+        if (this.initNumberSides % 2 !== 0) {
+            for (let i = 0; i < this.initNumberSides; i++) {
                 ctx.lineTo(
-                    startX + radiusWithin * Math.cos(ANGLE_EVEN * i - (PolygoneConstants.ANGLE_ODD / PolygoneConstants.ANGLE_LONG) * Math.PI),
-                    startY + radiusWithin * Math.sin(ANGLE_EVEN * i - (PolygoneConstants.ANGLE_ODD / PolygoneConstants.ANGLE_LONG) * Math.PI),
+                    this.centerX +
+                        this.radiusWithin * Math.cos(ANGLE_EVEN * i - (PolygoneConstants.ANGLE_ODD / PolygoneConstants.ANGLE_LONG) * Math.PI),
+                    this.centerY +
+                        this.radiusWithin * Math.sin(ANGLE_EVEN * i - (PolygoneConstants.ANGLE_ODD / PolygoneConstants.ANGLE_LONG) * Math.PI),
                 );
             }
         } else {
-            ctx.moveTo(startX + radiusWithin, startY);
-            for (let i = 0; i < sides; i++) {
-                ctx.lineTo(startX + radiusWithin * Math.cos(ANGLE_EVEN * i), startY + radiusWithin * Math.sin(ANGLE_EVEN * i));
+            ctx.moveTo(this.centerX + this.radiusWithin, this.centerY);
+            for (let i = 0; i < this.initNumberSides; i++) {
+                ctx.lineTo(this.centerX + this.radiusWithin * Math.cos(ANGLE_EVEN * i), this.centerY + this.radiusWithin * Math.sin(ANGLE_EVEN * i));
             }
         }
         ctx.closePath();
-        ctx.strokeStyle = secondaryColor;
-        ctx.lineWidth = lineWidth;
+        ctx.strokeStyle = this.borderColor;
+        ctx.lineWidth = this.lineWidth;
         ctx.stroke();
-        if (fillMethod !== ToolConstants.FillMode.OUTLINE) {
-            ctx.fillStyle = primaryColor;
+        if (this.fillMode !== ToolConstants.FillMode.OUTLINE) {
+            ctx.fillStyle = this.primaryColor;
             ctx.fill();
         }
     }
 
-    private getPolygoneCenter(start: Vec2, end: Vec2): Vec2 {
+    private getPolygoneCenter(start: Vec2, end: Vec2): void {
         let displacementX: number;
         let displacementY: number;
         const radiusX = Math.abs(end.x - start.x) / 2;
@@ -95,18 +88,16 @@ export class PolygoneCommand extends Command {
 
         const xVector = end.x - start.x;
         const yVector = end.y - start.y;
-        const centerX = start.x + Math.sign(xVector) * displacementX;
-        const centerY = start.y + Math.sign(yVector) * displacementY;
-
-        return { x: centerX, y: centerY };
+        this.centerX = start.x + Math.sign(xVector) * displacementX;
+        this.centerY = start.y + Math.sign(yVector) * displacementY;
     }
 
-    getRadiiXAndY(path: Vec2[]): number[] {
+    getRadiiX(path: Vec2[]): number {
         let xRadius = Math.abs(path[PolygoneConstants.END_INDEX].x - path[PolygoneConstants.START_INDEX].x) / 2;
-        let yRadius = Math.abs(path[PolygoneConstants.END_INDEX].y - path[PolygoneConstants.START_INDEX].y) / 2;
+        const yRadius = Math.abs(path[PolygoneConstants.END_INDEX].y - path[PolygoneConstants.START_INDEX].y) / 2;
         const shortestSide = Math.min(Math.abs(xRadius), Math.abs(yRadius));
-        xRadius = yRadius = shortestSide;
+        xRadius = shortestSide;
 
-        return [xRadius, yRadius];
+        return xRadius;
     }
 }
