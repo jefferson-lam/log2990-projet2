9be0d7286440624433a8e8faf3a1f2b5ea1b9e20
Hee-MinYang <hee-min.yang@polymtl.ca>
2021-04-19 16:58:17 -0400

    MainPageComponent: Removed useless property visible

--- client/src/app/components/main-page/main-page-carrousel/main-page-carrousel.component.ts
+++ client/src/app/components/main-page/main-page-carrousel/main-page-carrousel.component.ts
@@ -28,7 +28,6 @@ export class MainPageCarrouselComponent {
     serverErrorMessage: string;
     deleted: boolean;
     noValidDrawing: boolean;
-    visible: boolean;
     selectable: boolean;
     removable: boolean;
     deleteClick: boolean;
@@ -56,7 +55,6 @@ export class MainPageCarrouselComponent {
         this.serverErrorMessage = '';
         this.deleted = false;
         this.noValidDrawing = false;
-        this.visible = true;
         this.selectable = true;
         this.removable = true;
         this.separatorKeysCodes = [ENTER, COMMA];
