f939ba5f2a53b3adea4e2322085b75b69ca468b2
Danny <danny.tran@polymtl.ca>
2021-04-18 23:41:17 -0400

    Save pop-up: added new tests for refactored tag methods

--- client/src/app/components/sidebar/save-drawing-page/tag-input/tag-input.component.spec.ts
+++ client/src/app/components/sidebar/save-drawing-page/tag-input/tag-input.component.spec.ts
@@ -66,6 +66,20 @@ describe('TagValidatorComponent', () => {
         expect(component.isSavePossible).toEqual(true);
     });
 
+    it('distinctTagsValidator should set distinctTagsDivClass to "Failed" if tag already exists in tags', () => {
+        const testTag = 'test';
+        component.tags.push(testTag);
+        component.distinctTagsValidator(testTag);
+        expect(component.distinctTagsDivClass).toEqual('Failed');
+        expect(component.unsatisfiedRequirements).not.toEqual(0);
+    });
+
+    it("distinctTagsValidator should set distinctTagsDivClass to 'Satisfied' if tag doesn't exists in tags", () => {
+        const testTag = 'test';
+        component.distinctTagsValidator(testTag);
+        expect(component.distinctTagsDivClass).toEqual('Satisfied');
+    });
+
     it('validateTag should set minLengthDivClass to "Failed" if tag is shorter than minimum allowed', () => {
         const testTag = '';
         component.validateTag(testTag);
@@ -78,6 +92,19 @@ describe('TagValidatorComponent', () => {
         component.validateTag(testTag);
         expect(component.distinctTagsDivClass).toEqual('Satisfied');
         expect(component.isSavePossible).toEqual(true);
+    });
+
+    it('minLengthTagsValidator should set minLengthDivClass to "Failed" if tag is shorter than minimum allowed', () => {
+        const testTag = '';
+        component.minLengthTagsValidator(testTag);
+        expect(component.minLengthDivClass).toEqual('Failed');
+        expect(component.unsatisfiedRequirements).not.toEqual(0);
+    });
+
+    it('minLengthTagsValidator should set minLengthDivClass to "Satisfied" if tag length is equal or longer than minimum allowed', () => {
+        const testTag = 't';
+        component.minLengthTagsValidator(testTag);
+        expect(component.minLengthDivClass).toEqual('Satisfied');
     });
 
     it('validateTag should set maxLengthDivClass to "Failed" if tag is longer than maximum allowed', () => {
@@ -94,6 +121,19 @@ describe('TagValidatorComponent', () => {
         expect(component.isSavePossible).toEqual(true);
     });
 
+    it('maxLengthTagsValidator should set maxLengthDivClass to "Failed" if tag is longer than maximum allowed', () => {
+        const testTag = '012345678901234567891';
+        component.maxLengthTagsValidator(testTag);
+        expect(component.maxLengthDivClass).toEqual('Failed');
+        expect(component.unsatisfiedRequirements).not.toEqual(0);
+    });
+
+    it('maxLengthTagsValidator should set maxLengthDivClass to "Satisfied" if tag is shorter than maximum allowed', () => {
+        const testTag = '012345678901';
+        component.maxLengthTagsValidator(testTag);
+        expect(component.maxLengthDivClass).toEqual('Satisfied');
+    });
+
     it('validateTag should set noSpecialCharactersDivClass to "Failed" if tag has special characters', () => {
         const testTag = '😂';
         component.validateTag(testTag);
@@ -106,6 +146,19 @@ describe('TagValidatorComponent', () => {
         component.validateTag(testTag);
         expect(component.noSpecialCharacterDivClass).toEqual('Satisfied');
         expect(component.isSavePossible).toEqual(true);
+    });
+
+    it('characterTagsValidator should set noSpecialCharactersDivClass to "Failed" if tag has special characters', () => {
+        const testTag = '😂';
+        component.validateTag(testTag);
+        expect(component.noSpecialCharacterDivClass).toEqual('Failed');
+        expect(component.unsatisfiedRequirements).not.toEqual(0);
+    });
+
+    it("characterTagsValidator should set noSpecialCharactersDivClass to 'Satisfied' if tag doesn't have special characters", () => {
+        const testTag = 'NoLaughingAllowed';
+        component.validateTag(testTag);
+        expect(component.noSpecialCharacterDivClass).toEqual('Satisfied');
     });
 
     it('validateTag should set maxTagsCountDivClass to "Failed" if tag already has max amount of tags allowed', () => {
@@ -127,6 +180,42 @@ describe('TagValidatorComponent', () => {
         component.validateTag(testTag);
         expect(component.maxTagsCountDivClass).toEqual('Satisfied');
         expect(component.isSavePossible).toEqual(true);
+    });
+
+    it('maxTagsValidator should set maxTagsCountDivClass to "Failed" if tag already has max amount of tags allowed', () => {
+        const testTag = 'test';
+        for (let i = 0; i < DatabaseConstants.MAX_TAGS_COUNT; i++) {
+            component.tags.push(testTag + i.toString());
+        }
+        component.maxTagsValidator();
+        expect(component.maxTagsCountDivClass).toEqual('Failed');
+        expect(component.unsatisfiedRequirements).not.toEqual(0);
+    });
+
+    it("maxTagsValidator should set maxTagsCountDivClass to 'Satisfied' if tag doesn't have max amount of tags allowed", () => {
+        const testTag = 'test';
+        const RANDOM_AMOUNT = 10;
+        for (let i = 0; i < RANDOM_AMOUNT; i++) {
+            component.tags.push(testTag + i.toString());
+        }
+        component.maxTagsValidator();
+        expect(component.maxTagsCountDivClass).toEqual('Satisfied');
+    });
+
+    it('validateTag should call all validators methods', () => {
+        const distinctSpy = spyOn(component, 'distinctTagsValidator');
+        const minLengthSpy = spyOn(component, 'minLengthTagsValidator');
+        const maxLengthSpy = spyOn(component, 'maxLengthTagsValidator');
+        const characterSpy = spyOn(component, 'characterTagsValidator');
+        const maxSpy = spyOn(component, 'maxTagsValidator');
+        const testTag = 'test';
+
+        component.validateTag(testTag);
+        expect(distinctSpy).toHaveBeenCalled();
+        expect(minLengthSpy).toHaveBeenCalled();
+        expect(maxLengthSpy).toHaveBeenCalled();
+        expect(characterSpy).toHaveBeenCalled();
+        expect(maxSpy).toHaveBeenCalled();
     });
 
     it('addTag should handle case where tag already exists in tags', () => {
